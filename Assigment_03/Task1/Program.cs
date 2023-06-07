95b189f486753685648fa12b4ff5016840b7bf22
Anri Managadze <amanagadze@tbcbank.com.ge>
2023-06-07 13:37:06 +0400

    Assigment_03_V1

---
+++ Assigment_03/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/* 1. დაწერეთ პროგრამა რომელსაც კონსოლიდან გადავცემთ რიცხვს
+      და პასუხად დაბეჭდავს რიცხვი მარტივია თუ შედგენილი. */
+
+int input = Convert.ToInt32(Console.ReadLine());
+int gamyofi = 1;
+int count = 0;
+
+while (input >= gamyofi)
+{
+    if (input % gamyofi == 0)
+    {
+        count++;
+    }
+    gamyofi++;
+}
+string result = count == 2 ? $"{input} is Prime Number" : $"{input} is Composed Number";
+Console.WriteLine(result);
