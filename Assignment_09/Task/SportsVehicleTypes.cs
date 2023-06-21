e5b06a996d64c59e0dad22a4878cd82bcd0bba80
Anri Managadze <amanagadze@tbcbank.com.ge>
2023-06-21 17:53:49 +0400

    Assignment_09_V1

---
+++ Assignment_09/Task/SportsVehicleTypes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class FormulaCar : SportsVehicle
+    {
+        private int _numberOfSeats;
+        public int NumberOfSeats { get; set; }
+
+        public FormulaCar(string motor, string wheel, string color, int maxSpeed,int numberOfSeats)
+            :base(motor, wheel, color, maxSpeed)
+        {
+            _numberOfSeats = numberOfSeats;
+        }
+    }
+
+    public class OffroadCar : SportsVehicle
+    {
+        private bool _bigTire;
+        public bool BigTire { get;set; }
+
+        public OffroadCar(string motor, string wheel, string color, int maxSpeed, bool bigTire)
+            : base(motor, wheel, color, maxSpeed)
+        {
+            _bigTire = bigTire;
+        }
+    }
+}
