d7d6814c8e509f4eec89fb88f055c058010893ce
anri-managadze <81805368+anri-managadze@users.noreply.github.com>
2023-06-28 16:22:27 +0400

    Add files via upload

---
+++ Assignment_11/Task2/Storage.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Storage<T>
+    {
+        private T[] _x;
+
+        private int count = 0;
+        public Storage(int size)
+        {
+            _x = new T[size];
+        }
+        public void Add(T temp)
+        {
+            if (count < _x.Length)
+            {
+                _x[count] =temp;
+                count++;
+            }
+            Console.WriteLine($" Added : {temp} ,");
+        }
+
+        public void Remove(int index)
+        {
+            if (index >= 0)
+            {
+                Array.Clear(_x, index, 1);
+                count--;
+                Console.WriteLine("Element was Deleted: " + index); 
+            }       
+        }
+        public void Update(int index, T item)
+        {
+            if (index >= 0 && index < count)
+                _x[index] = item;
+                Console.WriteLine("Element Update:" + item);
+        }
+    }
+}
---
+++ Assignment_11/Task2/Test.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Test
+    {
+        Storage<string> storage1 = new Storage<string>(3);
+        
+        [Benchmark]
+        public void Add()
+        {
+            storage1.Add("hey");
+            storage1.Add("yes");
+            storage1.Add("no");
+        }
+        [Benchmark]
+        public void Remove()
+        {
+            storage1.Remove(1);
+        }
+        [Benchmark]
+        public void Update()
+        {
+            storage1.Update(0, "hi");
+        }
+    }
+}
