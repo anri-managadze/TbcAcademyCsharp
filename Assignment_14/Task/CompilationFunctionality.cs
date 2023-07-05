4f72b6b91b7a25c2b3aea4e49c509b5b6ad93f82
AManagadze <AManagadze@L_ANRMANAGADZE>
2023-07-05 18:02:34 +0400

    Assignment_14

---
+++ Assignment_14/Task/CompilationFunctionality.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class CompilationFunctionality
+    {
+        private const string QuizFileName = "quiz.txt";
+
+        public void ClearQuiz()
+        {
+            if (File.Exists(QuizFileName))
+            {
+                File.WriteAllText(QuizFileName,string.Empty);
+                Console.WriteLine("Quiz cleared successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Quiz file does not exist.");
+            }
+        }
+        public void AddQuestion()
+        {
+            Console.WriteLine("Enter Your question:");
+            string question = "Your Question: " +Console.ReadLine();
+
+            Console.WriteLine("Enter the answers (separated by commas):");
+            string[] answers = Console.ReadLine().Split(',');
+
+            Console.WriteLine("Enter the correct answer (index starting from 1):");
+            int correctAnswerIndex =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Question Point :");
+            string score = ("Question Point: " + Console.ReadLine());
+
+            var questionData = new List<string>
+        {
+            question,
+            string.Join(",", answers),
+            correctAnswerIndex.ToString(),
+            score
+        };
+
+            using (var stream = new StreamWriter(QuizFileName,true))
+            {
+                stream.WriteLine(string.Join("\n", questionData));
+            }
+            Console.WriteLine("Question added to the quiz.");
+        }
+    }
+}
+
---
+++ Assignment_14/Task/FillingFunctionality.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Task
+{
+    public class FillingFunctionality
+    {
+        public int Filling()
+        {
+            int score = 0;
+            string filePath = "quiz.txt";
+
+                using (StreamReader streamReader = new StreamReader(filePath))
+                {        
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {             
+                        Console.WriteLine(line);
+                        Console.WriteLine("Enter the correct answer:");
+
+                        string correctAnswer = streamReader.ReadLine();
+                        string userAnswer = Console.ReadLine();
+
+                        if (userAnswer == correctAnswer)
+                        {
+                            score++;
+                        }
+                    }
+            }
+            return score;
+        }
+    }
+}
