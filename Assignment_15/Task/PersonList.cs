5c268fc62c483124afa09187697ffee5babd9882
AManagadze <AManagadze@L_ANRMANAGADZE>
2023-07-07 17:32:51 +0400

    Assignment_15_V1

---
+++ Assignment_15/Task/PersonList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class PersonList
+    {
+        public List<Person> _list;
+        
+
+        public PersonList()
+        {
+            _list = new List<Person>();
+        }
+        public void AddPerson (Person person) {
+            _list.Add(person);
+             Console.WriteLine($"You added Person: Name:{person.FirstName}, LastName:{person.LastName}, Gender:{person.Gender}, Phone:{person.Phone}");         
+        }
+        public void GetPerson()
+        {
+            if (_list.Count > 0)
+            {
+                foreach (Person person in _list)
+                {
+                    Console.WriteLine($" Name:{person.FirstName}, LastName:{person.LastName}, Gender:{person.Gender}, Phone:{person.Phone}");
+                }
+            } else 
+            { 
+                Console.WriteLine("List is Empty"); 
+            }
+        }
+    }
+}
---
+++ Assignment_15/Task/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection;
+using System.Security;
+using Task;
+
+PersonList personList = new PersonList();
+CarList carList = new CarList();
+
+
+Console.WriteLine("Please choose Classes: ");
+Console.WriteLine("1.PersonList");
+Console.WriteLine("2.CarList");
+int input = int.Parse(Console.ReadLine());
+switch (input)
+{
+    case 1:
+        while (true)
+        {
+            Console.WriteLine("Choose Methods: ");
+            var type = typeof(PersonList);
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                if (method.DeclaringType == type)
+                {
+                    Console.WriteLine(method.Name);
+                }
+            }
+            Console.WriteLine("Exit");
+            string input1 = Console.ReadLine();
+            switch (input1)
+            {
+                case "AddPerson":
+                    Console.WriteLine("FirstName: ");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Lastname: ");
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine("Gender: ");
+                    string gender = Console.ReadLine();
+                    Console.WriteLine("Phone: ");
+                    string phone = Console.ReadLine();
+                    Person person = new Person(firstName, lastName, gender, phone);
+
+                    type.GetMethod("AddPerson").Invoke(personList,BindingFlags.Public | BindingFlags.Instance, null, new object?[]{ person }, null);
+                    break;
+                case "GetPerson":
+                    personList.GetPerson();
+                    break;
+                case "Exit":
+                    Console.WriteLine("Exit");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
+    case 2:
+        while (true)
+        {
+            Console.WriteLine("Choose Methods: ");
+            var type = typeof(CarList);
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                if (method.DeclaringType == type)
+                {
+                    Console.WriteLine(method.Name);
+                }
+            }
+            Console.WriteLine("Exit");
+            string input1 = Console.ReadLine();
+            switch (input1)
+            {
+                case "AddCar":
+                    Console.WriteLine("Model: ");
+                    string model = Console.ReadLine();
+                    Console.WriteLine("Type: ");
+                    string cartype = Console.ReadLine();
+                    Console.WriteLine("Color: ");
+                    string color = Console.ReadLine();
+                   
+                    Car car = new Car(model, cartype, color);
+                    type.GetMethod("AddCar").Invoke(carList, BindingFlags.Public | BindingFlags.Instance, null, new object?[] { car }, null);
+                    break;
+                case "GetCar":
+                    carList.GetCar();
+                    break;
+                case "Exit":
+                    Console.WriteLine("Exit");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
+        break;
+    default:
+        Console.WriteLine("");
+        break;
+}
